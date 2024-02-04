7a805de06894287ba6dee552a7e65cbec691826f
AndreasSitumorang <andreassitumorang098@gamil.com>
2024-02-04 20:49:17 +0700

    learn golang

---
+++ WebApplication/GolangBackEnd/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+func main() {
+fmt.Println("heloo fellwsas")
+
+//normal Way
+var nameOne string = "Joma"
+var nameTwo = "Tech" 
+var nameThree string
+
+fmt.Println(nameOne, nameTwo, nameThree)
+
+nameOne = "Marshal"
+nameThree = "Doni"
+nameTwo = "Luckas"
+fmt.Println(nameOne, nameTwo, nameThree)
+
+//short way --note  can't use outside function
+nameShort := "Samsung"
+fmt.Println(nameShort)
+
+}
