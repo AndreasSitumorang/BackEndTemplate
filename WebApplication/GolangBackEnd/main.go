a4cdef25ef9875c09583dbef2f3fa39b7b408296
AndreasSitumorang <andreassitumorang098@gamil.com>
2024-02-05 20:24:12 +0700

    learn go lang basic

--- WebApplication/GolangBackEnd/main.go
+++ WebApplication/GolangBackEnd/main.go
@@ -20,4 +20,28 @@ fmt.Println(nameOne, nameTwo, nameThree)
 nameShort := "Samsung"
 fmt.Println(nameShort)
 
+
+// ints
+
+var numberOne int = 26
+var numberTwo = 20
+numberThree := 29
+
+fmt.Println(numberOne, numberTwo, numberThree)
+
+
+// print (formated string) 
+var nameJoma string = "joam_tech"
+var ages int = 26
+
+fmt.Printf("nama saya %v dan usia saya adalah %v \n",nameJoma,ages)
+fmt.Printf("nama saya %q dan usia saya adalah %q \n",nameJoma,ages)
+fmt.Printf("type is %T \n", ages)
+fmt.Printf("your scored %f \n", 255.555)
+fmt.Printf("your scored %0.2f \n", 255.555)
+
+// sprintf (save formatted strings)
+//https://pkg.go.dev/fmt
+var testStr = fmt.Sprintf("nama saya %v dan usia saya adalah %v \n",nameJoma,ages)
+fmt.Println(testStr)
 }
