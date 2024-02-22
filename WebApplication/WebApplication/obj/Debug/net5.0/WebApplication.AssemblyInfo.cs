608ca4f2dc93709a0e35f34c251fd6ba7abc8741
AndreasSitumorang <andreassitumorang098@gamil.com>
2024-02-22 18:33:57 +0700

    add frontend

--- WebApplication/WebApplication/obj/Debug/net5.0/WebApplication.AssemblyInfo.cs
+++ WebApplication/WebApplication/obj/Debug/net5.0/WebApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3158cc56415b0adc629fa9ff547b1dc54e95d602")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
