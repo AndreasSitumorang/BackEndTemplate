7eba9bac46344cb7b5ab9b1d4c13be38086f252b
AndreasSitumorang <andreassitumorang098@gamil.com>
2024-02-23 22:11:15 +0700

    add module

--- WebApplication/GolangBackEnd/models/Users.go
+++ WebApplication/GolangBackEnd/models/Users.go
@@ -1,12 +1,14 @@
 package models
 
 type Account struct {
-    user_id   int    `json:"user_id"`
-    username  string `json:"username"`
-    password  string `json:"password"`
-    key       string `json:"key"`
-    salt      string `json:"salt"`
-    created_at string `json:"created_at"`
-    updated_at string `json:"updated_at"`
-    email     string `json:"email"`
+    User_id   int    `json:"user_id"`
+    Username  string `json:"username"`
+    Password  string `json:"password"`
+    Key       string `json:"key"`
+    Salt      string `json:"salt"`
+    Created_at string `json:"created_at"`
+    Updated_at string `json:"updated_at"`
+    Email     string `json:"email"`
 }
+
+//Adding model use capilta latter
